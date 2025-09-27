eeeb02faf54bd77199216edaa0e371c774e663cf
xytx <3232076199@qq.com>
2025-09-27 22:54:15 +0800

    mkdir user_contrib

--- cpp/batch_cal.sh
+++ cpp/batch_cal.sh
@@ -10,8 +10,8 @@ files=`ls ../srf_data/*.txt`
 # 筛选landsat系列
 files=`find ../srf_data -type f -name "*land*.txt"`
 
-# GNU parallel并行
-parallel --line-buffer ./build/main_ecw {} ::: $files
+# # GNU parallel并行
+# parallel --line-buffer ./build/main_ecw {} ::: $files
 
-# # =====> 使用gmt进行出图 <=====
-# parallel --line-buffer ./gmt_plot.sh {} ::: $files
+# =====> 使用gmt进行出图 <=====
+parallel --line-buffer ./gmt_plot.sh {} ::: $files
